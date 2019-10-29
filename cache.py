21d46c1ceef53a08770f1c0d25a4735c5ec38c18
earacena <30157858+earacena@users.noreply.github.com>
2019-10-29 15:15:47 -0400

    Added Block structure for use in Cache component.

--- cache.py
+++ cache.py
@@ -3,6 +3,18 @@
 # Filename: cache.py
 # File Description: This file contains the functions related to the Cache component
 
+class Block:
+    def __init__(self):
+        self.ref_bit = 0
+        self.tag = 0
+        self.index = 0
+        self.data = None
+
 class Cache:
     def __init__(self):
         pass
+
+     # Least Recently Used algorithms
+      # insert
+      # remove
+      # find
