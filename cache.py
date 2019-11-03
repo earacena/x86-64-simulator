ea8fb883bbf840c68693fa700fa965ea6e19380b
earacena <30157858+earacena@users.noreply.github.com>
2019-11-03 01:09:00 -0400

    Implemented insertion function for Cache with working unit test.
    Cache will now store blocks as individual blocks and do additional work to loop in
    pairs.
    As a result of a bug with iteration over Set objects, the Cache component iterates over
    blocks in pairs. When attempting to insert the first element into the first block
    of a two-set associative cache, all of the first blocks in every set where changed.
    Effectively preventing more than two elements from entering cache.

--- cache.py
+++ cache.py
@@ -9,17 +9,17 @@ class Block:
         self.valid = 0
         self.tag = 0
         self.index = 0
-        self.data = None      # data requested by CPU, for example: value of DWORD PTR [rbp-4]
+        self.data = "No data here"      # data requested by CPU, for example: value of DWORD PTR [rbp-4]
 
 
 ## Two-way associative set
-class Set:
-    def __init__(self):
-        self.block1 = Block()
-        self.block2 = Block()
+#class Set:
+#    def __init__(self):
+#        self.block1 = Block()
+#        self.block2 = Block()      
 
 class Cache:
-    def __init__(self, cache_size, block_size, num_of_sets, debug):
+    def __init__(self, cache_size, block_size, debug):
         self.debug_info = debug
 
         if self.debug_info == True:
@@ -28,35 +28,86 @@ class Cache:
         ### Initialization code ###
         self.cache_size  = cache_size
         self.block_size  = block_size
-        self.num_of_sets = num_of_sets 
-
+        #self.num_of_sets = num_of_sets 
+        self.num_of_blocks_used = 0
+        self.max_number_of_blocks = int(self.cache_size / self.block_size)
         # set (10 bytes)  - block 1 (5 bytes) | block 2 (5 bytes)
-        self.cache = [None] * int(((self.cache_size / self.block_size) / self.num_of_sets))
+        self.cache = [Block()] * int(self.cache_size / self.block_size)
         ###########################
         
             
         if self.debug_info == True:
             print("[Cache] finished initializing...")
+            print("[Cache] Max number of blocks '", self.max_number_of_blocks, "'...")
+    
+    # return true if full, false otherwise
+    def is_full(self):
+        return self.num_of_blocks_used == self.max_number_of_blocks
 
-    # Least Recently Used algorithms
-    # insert, insert data into a block in position of previous LRU
-    def insert(self, data, position): 
-        if self.debug_info == True:
-            print("[Cache] inserting block in LRU position:")
-            print("[Cache]        Data: ", data)
-            print("[Cache]    Position: ", position)
-
-
-    # LRU , return true if least recently used block
+    # LRU , return least recently used block's position
     def LRU(self):
         if self.debug_info == True:
             print("[Cache] Looking for least recently used block...")
+
+        oldest_time = 0
+        oldest_block_number = None
+
+        for index, block in enumerate(self.cache):
+            if block.timer >= oldest:
+                oldest = set.block1.timer
+                oldest_block_number = index
+                
+        # Once oldest time is marked, find first block with that time
+         
         
         # Look in cache for LRU
         if self.debug_info == True:
             print("[Cache] LRU block found...")
-            # print block.data, block timer.
+            print("[...]      block data : ", self.cache[oldest_block_number].data)
+            print("[...]      block timer: ", self.cache[oldest_block_number].timer)
         
+        return oldest_block_number
+
+    # Least Recently Used algorithms
+    # insert, insert data into a block in position of previous LRU
+    def insert(self, data): 
+        if self.debug_info == True:
+            print("[Cache] inserting block...")
+            print("[...]        Data: ", data)
+            print("[...] Current Cache table: ")
+            self.print_cache()
+
+        # swap blocks
+        if self.is_full() == True:
+            print("[...] Cache full...")
+            lru_block = self.LRU()
+
+
+            if self.debug_info == True:
+                print("[Cache] LRU block to be replaced: ", )
+                print("[...]    data: ", lru_block.data)
+                print("[...]   timer: ", lru_block.timer) 
+            
+            self.cache[lru_block] = Block()
+            self.cache[lru_block].valid = 1
+            self.cache[lru_block].data = data
+
+            self.print_cache()
+        # find empty spot
+        else:
+            spot = False
+            print("[...] Cache has empty slots...")
+            position = self.find_empty_spot()
+            self.cache[position] = Block()
+            self.cache[position].data = data
+            self.cache[position].valid = 1
+            
+            
+
+    def find_empty_spot(self):
+        for index, block in enumerate(self.cache):
+            if block.valid == 0:
+                return index            
 
     # returns "HIT" or "MISS" depending on if data is present
     def find_block(self, data):
@@ -68,13 +119,47 @@ class Cache:
         if self.debug_info == True:
             print("[Cache] Replacing LRU with new block...")
 
-
+    def update_timer(self):
+        for block in self.cache:
+            block.timer = block.timer + 1
+    def print_cache(self):
+        counter = 0
+        print("[...] Set | B1 timer | B1 data\t| B2 timer | B2 data\t")
+        it = iter(self.cache)
+        for block in it:
+              block2 = next(it)
+              print("[...] ", counter, " | " , block.timer, " | ", block.data, " | ",
+                    block2.timer, " | ", block2.data)
+            
 
 def main():
     debug = True
     cache_size  = 50       # smaller than main memory
     block_size  = 5        # smaller than cache, two blocks per set
-    num_of_sets = 2
 
-    cache = Cache(cache_size, block_size, num_of_sets, debug)
+    cache = Cache(cache_size, block_size, debug)
 
+    # populate the cache with data
+
+    cache.insert(["0xAAAA", 100])
+    cache.update_timer()
+    cache.insert(["0xBBBB", 200])
+    cache.update_timer()
+    cache.insert(["0xCCCC", 300])
+    cache.update_timer()
+    cache.insert(["0xDDDD", 400])
+    cache.update_timer()
+    cache.insert(["0xEEEE", 500])
+    cache.update_timer()
+    cache.insert(["0xFFFF", 600])
+    cache.update_timer()
+    cache.insert(["0x0000", 700])
+    cache.update_timer()
+    cache.insert(["0x1111", 800])
+    cache.update_timer()
+    cache.insert(["0x2222", 900])
+    cache.update_timer()
+    cache.insert(["0x3333", 1000])
+    cache.update_timer()
+
+    cache.print_cache()
