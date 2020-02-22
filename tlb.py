dc1c45986172193cb2eba896f86be71a4a65a67c
earacena <30157858+earacena@users.noreply.github.com>
2020-02-22 16:19:14 -0500

    Update tlb.py

--- tlb.py
+++ tlb.py
@@ -1,6 +1,6 @@
 """
     Project: x86_64 Hardware Simulator
-    Author: Emanuel Aracena
+    Author: Emanuel Aracena Beriguete
     Filename: tlb.py
     File Description: This file contains the implementation of the TLB component
 """
